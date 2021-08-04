678946fe76f09efe1a301b1776595c2609ede7e7
cauvery-digital <81048372+cauvery-digital@users.noreply.github.com>
2021-08-05 01:19:12 +0530

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Conversation Chemistry</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="/bootstrapwebsite/bootstrap-greg.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script src='//cbtb.clickbank.net/?vendor=meetys'></script>
+</head>
+<body>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-219227-28']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script> 
+
+<div class="container-fluid">
+
+<img src="/conversationchemistry/images/conchemheaderw.png" alt="" class="img-responsive centered" >
+
+
+
+<div class="whiteouter">
+
+
+<img src="/conversationchemistry/women/images/conchemwom.gif" class="img-responsive centered" style="margin-top:15px;margin-bottom:10px;">
+
+
+<div class="row nomargin">
+<div class="col-sm-12"><p class="times14pt text-center">Make sure your sound is turned on. Please wait up to 10 seconds for the video to load</p></div>
+</div><!-- end row-->
+
+<div  style="max-width:790px;max-height: 520px;margin-left:auto;margin-right:auto;border: #6155a1 solid 2px;background-color: #fff;margin-bottom:5px;">
+<style>.embed-responsive{position:relative;display:block;height:0;padding:0;overflow:hidden}.embed-responsive .embed-responsive-item,.embed-responsive iframe,.embed-responsive embed,.embed-responsive object{position:absolute;top:0;bottom:0;left:0;width:100%;height:100%;border:0}.embed-responsive.embed-responsive-4by3{padding-bottom:56.25%}</style>
+<!--<div class="embed-responsive embed-responsive-4by3"><iframe src="/yt/index.php?id=SbmJymrKAUg" scrolling="no"></iframe></div>-->
+<div class="embed-responsive embed-responsive-4by3">
+<iframe src="//fast.wistia.net/embed/iframe/663udv1nca?videoFoam=true" allowtransparency="true" scrolling="no" class="wistia_embed embed-responsive-item" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen"></iframe><script src="//fast.wistia.net/assets/external/iframe-api-v1.js"></script>
+</div>
+</div><!-- end row -->
+<p class="times10pt text-center"><br><strong>Warning: If you've ever struggled to talk to or create lasting attraction with men, then you NEED to watch this video right now.</strong><br><br> 
+Remember: Watch the entire video, as the end will surprise you!<br><br><br></p>
+
+<div class="row nomargin"><div class="col-sm-12">
+</div></div><!-- end row and col-->
+
+
+
+
+<script type="text/javascript">
+			    function showIt() {
+			      document.getElementById("hid").style.display= "";
+			    }
+			    setTimeout("showIt()", 1468000); // 1000 = 1 sec
+</script>
+
+<div id="hid" style="display:"><!-- timer div begins here-->
+
+
+
+<div class="row" style="max-width: 850px; margin-left:auto;margin-right:auto;">
+<div class="col-sm-5">
+<img src="/conversationchemistry/images/conchemwstarburst.png" class="img-responsive" style="margin-left: 5%;">
+</div>
+<div class="col-sm-7">
+
+<div class="row"> 
+<h2>Just <span style="color:red;">1 Payment of $47</span> if you act today!<br><br></h2>
+</div><!-- end nested row-->
+
+<div class="row">
+<div class="col-sm-9 col-xs-12">
+<a href="order-responsive.php">Click Here To Signup Now</a><br>
+<a href="order-responsive.php"><img src="/conversationchemistry/images/addtocart2.png" class="img-responsive"></a>
+</div>
+<div class="col-sm-3 hidden-xs-down">
+<img src="/conversationchemistry/images/satisfy.png" class="img-responsive hidden-xs">
+</div>
+</div><!-- end nested row-->
+
+</div>
+</div><!-- end row-->
+
+
+
+
+<div style="max-width: 900px;margin-left:auto;margin-right:auto;">
+<div class="row" style="background: url(//www.meetyoursweet.com/images/guaranteedivtop.png) repeat-x; width: 100%; margin-left:auto;margin-right:auto;padding-top: 20px;margin-top:20px;margin-bottom: 10px;">
+<div class="col-sm-8 col-xs-12"><img src="/images/conchguaranteesticker.png" class="img-responsive"></div>
+
+			<div class="table-responsive" style="border: 0px !important;">
+				<table style="max-width: 80%;margin-left:auto;margin-right:auto;border:0;" border="0" align="center" cellspacing="0">
+					<tr>
+				<td style="padding:7px;"><img src="//d1f5iw4g082q78.cloudfront.net/bibseal.gif" class="img-responsive" alt="BIB seal"></td>
+				<td style="padding:7px;"><a href="#" onclick="window.open('https://www.sitelock.com/verify.php?site=meetyoursweet.com','SiteLock','width=600,height=600,left=160,top=170');" ><img alt="website security" title="SiteLock"  src="//shield.sitelock.com/shield/meetyoursweet.com" class="img-responsive"></a></td>
+					</tr>
+				</table>
+			</div><!-- end table-responsive-->
+
+
+</div><!-- end row-->
+<div class="row"><br>
+<p class="times12pt text-center" style="max-width: 95%;margin-left:auto;margin-right:auto;">Your order is backed by my 60-Day, No-Questions-Asked, 100% Satisfaction Guarantee.<br>
+If you are not 100% satisfied with your purchase, simply contact support@meetyoursweet.com within 60 days for a full refund.</p>
+</div><!-- end row-->
+<div class="row" style="height: 20px; background: url(//www.meetyoursweet.com/images/guaranteedivbot.png) repeat-x; width: 100%; margin-left:auto;margin-right:auto;">
+</div>
+</div>
+
+</div><!-- end timer div-->
+
+
+
+<br><br>
+</div><!-- end white div-->
+
+
+
+
+
+
+<!-- begin footer-->
+<br><br><br><br>
+<div class="clearfix"></div>
+<div class="row nomargin" style="padding-top:10px;padding-bottom:10px;">
+<div style="max-width: 984px; margin:auto;">
+
+
+<div style="" class="footerlinks">
+Copyright 2021 <a href="http://meetyoursweet.com">MeetYourSweet.com</a> &copy;
+</div>
+</div>
+
+</div><!-- end row-->
+
+
+<!-- end footer-->
+</div>
+
+<!-- Start of ExitSplash.com Code -->
+<script language="javascript">
+var exitsplashmessage = "Wait! Before you go, we have an EXCLUSIVE Free Report for you...\n\n\n'The Top 6 Secrets to Attracting Men'\n\n\nDon't miss this.\n\nClick the *Stay on Page* button on the NEXT window to receive this report.";
+var exitsplashmessage = "\n\n Discover the secrets about men and what he's thinking... and never feel shut out or ignored ever again! Get my FREE 6-Part Mini Course on all the essential secrets to getting and keeping Mr Right... and making sure you're the only woman he wants and craves!\n\n";
+var exitsplashpage = 'newsexitw.php';
+</script>
+<script language="javascript" src="/exitsplash.php"></script>
+<!-- End of ExitSplash.com Code --> 
+</body>
+</html>
